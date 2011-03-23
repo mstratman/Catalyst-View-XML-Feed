9481738ea85a0d8a65fdbe9249798a9fe3b1942a
Mark A. Stratman <stratman@gmail.com>
2011-03-22 19:49:00 -0500

    added tests

--- t/lib/TestApp/Controller/Root.pm
+++ t/lib/TestApp/Controller/Root.pm
@@ -1,59 +1,130 @@
 package TestApp::Controller::Root;
 use Moose;
 use namespace::autoclean;
+use DateTime;
 
 BEGIN { extends 'Catalyst::Controller' }
 
 __PACKAGE__->config(namespace => q{});
 
-sub base : Chained('/') PathPart('') CaptureArgs(0) {}
+sub _feed_link :Private { return $_[1]->req->base; }
+sub _feed_base :Private { return $_[1]->req->base; }
+sub _feed_title :Private { return 'My awesome site'; }
+sub _feed_tagline :Private { return 'A great tagline'; }
+sub _feed_description :Private { return 'The greatest site ever'; }
+sub _feed_author :Private { return 'Mark A. Stratman'; }
+sub _feed_language :Private { return 'English'; }
+sub _feed_copyright :Private { return 'Copyright me 2011'; }
+sub _feed_generator :Private { return 'Catalyst::View::XML::Feed ' . $Catalyst::View::XML::Feed::VERSION; }
+sub _feed_id : Private { return _feed_link(@_); }
 
-# your actions replace this one
-sub main : Chained('base') PathPart('') Args(0) {
+sub _entry_title : Private { return 'My first post' }
+sub _entry_base : Private { $_[1]->uri_for('/', 'my_first_post'); }
+sub _entry_link : Private { $_[1]->uri_for('/', 'my_first_post'); }
+sub _entry_content : Private { return 'It works! And even more stuff here' }
+sub _entry_summary : Private { return 'It wor...' }
+sub _entry_category : Private { return 'Junk' }
+sub _entry_author : Private { return _feed_author(@_) }
+sub _entry_id : Private { return _entry_link(@_) }
+sub _now : Private { return DateTime->now }
+
+sub index : Path  {
     my ($self, $ctx) = @_;
     $ctx->res->body('<h1>It works</h1>');
 }
 
 sub string : Local {
     my ($self, $c) = @_;
-    $c->stash->{feed} = q{<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">
+    $c->stash->{feed} = q{<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0"
+ xml:base="http://localhost:3000/"
+ xmlns:blogChannel="http://backend.userland.com/blogChannelModule"
+ xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"
+ xmlns:content="http://purl.org/rss/1.0/modules/content/"
+ xmlns:atom="http://www.w3.org/2005/Atom"
+ xmlns:dcterms="http://purl.org/dc/terms/"
+>
 
-    <channel>
-    <title>My Feed Site</title>
-    <description>This is an example</description>
-    <link>http://www.example.com/link.htm</link>
-    <lastBuildDate>Mon, 28 Aug 2006 11:12:55 -0400 </lastBuildDate>
-    <pubDate>Tue, 29 Aug 2006 09:00:00 -0400</pubDate>
-    <item>
-    <title>It Works!</title>
-    <description>Yesss.... it works!</description>
-    <link>http://www.example.com/link.htm</link>
-    <guid isPermaLink="false"> 1102345</guid>
-    <pubDate>Tue, 29 Aug 2006 09:00:00 -0400</pubDate>
-    </item>
-    </channel>
-    </rss>};
+<channel>
+<title>My awesome site</title>
+<link>http://localhost:3000/</link>
+<description>A great tagline</description>
+<copyright>Copyright me 2011</copyright>
+<pubDate>Wed, 23 Mar 2011 00:34:40 -0000</pubDate>
+<webMaster>Mark A. Stratman</webMaster>
+<generator>Catalyst::View::XML::Feed 0.01</generator>
+
+<item>
+<title>My first post</title>
+<link>http://localhost:3000/my_first_post</link>
+<description>It wor...</description>
+<author>Mark A. Stratman</author>
+<category>Junk</category>
+<guid isPermaLink="true">http://localhost:3000/my_first_post</guid>
+<pubDate>Wed, 23 Mar 2011 00:34:40 -0000</pubDate>
+<content:encoded>It works! And even more stuff here</content:encoded>
+<dcterms:modified>2011-03-23T00:34:40Z</dcterms:modified>
+</item>
+</channel>
+</rss>};
     $c->forward('View::Feed');
 }
 
-sub xml_feed : Local {
-    my ($self, $c) = @_;
 
-    my $feed = XML::Feed->new('Atom');
-    $feed->id($c->req->base);
-    $feed->title('My Feed Site');
-    $feed->description('This is an example');
-    $feed->link($c->req->base);
-    $feed->modified(DateTime->now);
+sub _xml_feed : Private {
+    my ($self, $c, @format) = @_;
+
+    my $feed = XML::Feed->new(@format);
+    $feed->id($self->_feed_id($c));
+    $feed->title($self->_feed_title($c));
+    $feed->description($self->_feed_description($c));
+    $feed->link($self->_feed_link($c));
+    $feed->modified($self->_now($c));
+    $feed->base($self->_feed_base($c));
+    $feed->tagline($self->_feed_tagline($c));
+    $feed->author($self->_feed_author($c));
+    $feed->copyright($self->_feed_copyright($c));
+    $feed->generator($self->_feed_generator($c));
+
     my $entry = XML::Feed::Entry->new();
-    $entry->id('tag:example.com,' . time());
-    $entry->link($c->uri_for('rss_xml_feed', 'first_post')->as_string);
-    $entry->title('It Works!');
-    $entry->content('Yesss.... it works!');
+    $entry->title($self->_entry_title($c));
+    $entry->base($self->_entry_base($c));
+    $entry->link($self->_entry_link($c));
+    $entry->content($self->_entry_content($c));
+    $entry->summary($self->_entry_summary($c));
+    $entry->category($self->_entry_category($c));
+    $entry->author($self->_entry_author($c));
+    $entry->id($self->_entry_id($c));
+    $entry->issued($self->_now($c));
+    $entry->modified($self->_now($c));
     $feed->add_entry($entry);
 
-    $c->stash->{feed} = $feed;
+    return $feed;
+}
+
+sub atom_xml_feed : Local {
+    my ($self, $c) = @_;
+    $c->stash->{feed} = $self->_xml_feed($c, 'Atom');
+    $c->forward('View::Feed');
+}
+sub rss_xml_feed : Local {
+    my ($self, $c) = @_;
+    $c->stash->{feed} = $self->_xml_feed($c, 'RSS');
+    $c->forward('View::Feed');
+}
+sub rss09_xml_feed : Local {
+    my ($self, $c) = @_;
+    $c->stash->{feed} = $self->_xml_feed($c, 'RSS', version => '0.9');
+    $c->forward('View::Feed');
+}
+sub rss1_xml_feed : Local {
+    my ($self, $c) = @_;
+    $c->stash->{feed} = $self->_xml_feed($c, 'RSS', version => '1.0');
+    $c->forward('View::Feed');
+}
+sub rss2_xml_feed : Local {
+    my ($self, $c) = @_;
+    $c->stash->{feed} = $self->_xml_feed($c, 'RSS', version => '2.0');
     $c->forward('View::Feed');
 }
 
--- t/live-test.t
+++ t/live-test.t
@@ -17,14 +17,23 @@ my $mech = Test::WWW::Mechanize::Catalyst->new;
 $mech->get_ok('http://localhost/', 'get main page');
 $mech->content_like(qr/it works/i, 'main page has our text');
 
-$mech->get_ok('http://localhost/string', 'get /string');
-cmp_ok($mech->content_type, 'eq', 'text/xml', '/string has correct content_type');
-$mech->content_like(qr/it works/i, '/string has our text');
 
-#$mech->get_ok('http://localhost/perl_data', 'get xml from perl data');
-
-#die 'WTF:'. $mech->content_type;
-#$mech->content_like(qr/it works/i, 'see if it has our text');
-# TODO: write test cases for each possible type of 'entries'
+for my $action (qw(string atom_xml_feed rss_xml_feed rss09_xml_feed rss1_xml_feed rss2_xml_feed)) {
+    $mech->get_ok("http://localhost/$action", "get /$action");
+    my $ct = 'text/xml';
+    if ($action =~ /rss/) {
+        $ct = 'application/rss+xml';
+    } elsif ($action =~ /atom/) {
+        $ct = 'application/atom+xml';
+    }
+    cmp_ok($mech->content_type, 'eq', $ct, "/$action has correct content_type");
+    $mech->content_like(qr/my awesome site/i, "/$action has 'my awesome site'");
+    $mech->content_like(qr/my first post/i, "/$action has 'my first post'");
+    $mech->content_like(qr/my_first_post/i, "/$action has 'my_first_post'");
+    unless ($mech->content =~ m!http://my.netscape.com/rdf/simple/0.9/!i) {
+        $mech->content_like(qr/it works/i, "/$action has 'it works'");
+        $mech->content_like(qr/Mark A\. Stratman/i, "/$action has 'Mark A. Stratman'");
+    }
+}
 
 done_testing;
